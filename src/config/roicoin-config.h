b43a75051c6da79a0681e48dc3176ab4b0114873
ghobson2013 <ugac007@gmail.com>
2018-01-07 11:13:28 -0600

    bumping to version 1.1.4

--- src/config/roicoin-config.h
+++ src/config/roicoin-config.h
@@ -21,10 +21,10 @@
 #define CLIENT_VERSION_MINOR 1
 
 /* Build revision */
-#define CLIENT_VERSION_REVISION 3
+#define CLIENT_VERSION_REVISION 4
 
 /* Version is release */
-#define COPYRIGHT_YEAR 2017
+#define COPYRIGHT_YEAR 2018
 
 /* Define to 1 to enable wallet functions */
 #define ENABLE_WALLET 1
@@ -305,7 +305,7 @@
 #define PACKAGE_NAME "ROIcoin Core"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "ROIcoin Core 1.1.3"
+#define PACKAGE_STRING "ROIcoin Core 1.1.4"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "ROIcoin"
@@ -314,7 +314,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.1.3"
+#define PACKAGE_VERSION "1.1.4"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
