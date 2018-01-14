a3e27ba395fb1f7d0e9c9caf55756a7671ac842d
ROI_Coin <dev@roi-coin.com>
2018-01-14 12:46:08 -0600

    TestNet Chain 1142

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -163,13 +163,13 @@ public:
      	genesis.nTime    		 = 1515953759;  //date +%s
         genesis.nBits    		 = 0x21000FFF;  //0x21000FFF
 	genesis.nNonce 			 = 4294901760;  //0xffff0000
-        genesis.nStartLocation 		 = 259177;
-        genesis.nFinalCalculation 	 = 3482034946;
+        genesis.nStartLocation 		 = 187376;
+        genesis.nFinalCalculation 	 = 2495613843;
 	consensus.nPowAdjustmentInterval = 20 * 60;   	//Twenty minutes
 	consensus.nPowMidasBlockStart 	 = 1;		//Block Start MIDAS
 	consensus.nPowMidasTimeStart  	 = 1515953759;	//date +%s
 
-	
+/*	
 if(genesis.GetHash() != uint256S("008872e5582924544e5c707ee4b839bb82c28a9e94e917c94b40538d5658c04b") ){
             arith_uint256 hashTarget = arith_uint256().SetCompact(genesis.nBits);
             uint256 thash;
@@ -198,12 +198,12 @@ if(genesis.GetHash() != uint256S("008872e5582924544e5c707ee4b839bb82c28a9e94e917
             LogPrintf("block.nFinalCalculation = %u \n", genesis.nFinalCalculation);
 
         }
-	
+*/	
 	consensus.hashGenesisBlock = genesis.GetHash();
 	assert(consensus.hashGenesisBlock == genesis.GetHash());
 
-	//assert(consensus.hashGenesisBlock == uint256S("0x005566d40af96f9f09b5bd8b499f769e571ab11dea9390b4e81135f8770c63a4"));
-        //assert(genesis.hashMerkleRoot == uint256S("0x92feaffad16699d7f7acb5bb53e6fab02d3eb5751cc32a4f787b1cced8e24016"));
+	assert(consensus.hashGenesisBlock == uint256S("0x001d9814ad6ff3215353f740f3adff55b3f19c0b893c1c15fa69b1177364e62b"));
+        assert(genesis.hashMerkleRoot == uint256S("0x92feaffad16699d7f7acb5bb53e6fab02d3eb5751cc32a4f787b1cced8e24016"));
 
 
 	vSeeds.push_back(CDNSSeedData("TestNet.roi-coin.com", "TestNet.roi-coin.com"));  //TestNet - Toronto VPS
@@ -230,7 +230,7 @@ if(genesis.GetHash() != uint256S("008872e5582924544e5c707ee4b839bb82c28a9e94e917
 	{
             
 	boost::assign::map_list_of
-            ( 0, uint256S("001")),
+            ( 0, uint256S("0x001d9814ad6ff3215353f740f3adff55b3f19c0b893c1c15fa69b1177364e62b")),
 
             1515953759, // * UNIX timestamp of last checkpoint block
             1,   	// * total number of transactions between genesis and last checkpoint
@@ -241,7 +241,6 @@ if(genesis.GetHash() != uint256S("008872e5582924544e5c707ee4b839bb82c28a9e94e917
 };
 
 static CTestNetParams testNetParams;
-
 
 
 /**
