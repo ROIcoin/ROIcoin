aab3d9663b14f5d5c6f3b8115a0673f6656533a8
ghobson2013 <ugac007@gmail.com>
2018-01-14 14:46:20 -0600

    about page cleanup

--- src/init.cpp
+++ src/init.cpp
@@ -447,7 +447,9 @@ std::string HelpMessage(HelpMessageMode mode)
 
 std::string LicenseInfo()
 {
-    return FormatParagraph(strprintf(_("Copyright (C) 2009-%i The Bitcoin Core Developers"), COPYRIGHT_YEAR)) + "\n" +
+    return FormatParagraph(_("Download latest wallet: <https://roi-coin.com/roi-coin-downloads/>")) + "\n" +
+           "\n" +
+           FormatParagraph(strprintf(_("Copyright (C) 2009-%i The Bitcoin Core Developers"), COPYRIGHT_YEAR)) + "\n" +
            "\n" +
            FormatParagraph(strprintf(_("Copyright (C) 2017-%i The ROIcoin Developers"), COPYRIGHT_YEAR)) + "\n" +
            "\n" +
@@ -457,8 +459,7 @@ std::string LicenseInfo()
            "\n" +
            FormatParagraph(_("This product includes software developed by the OpenSSL Project for use in the OpenSSL Toolkit <https://www.openssl.org/> and cryptographic software written by Eric Young and UPnP software written by Thomas Bernard.")) +
            "\n" + "\n" + "\n" +"\n"+
-           FormatParagraph(_("Homepage: <https://roi-coin.com/>")) + "\n" + "\n"+
-           FormatParagraph(_("Download latest wallet: <https://roi-coin.com/roi-coin-downloads/>")) + "\n";
+           FormatParagraph(_("For more information please visit and join us at <https://roi-coin.com/>")) + "\n" + "\n";
 }
 
 static void BlockNotifyCallback(const uint256& hashNewTip)
