85a86cab485a5bc01cbb0238c58f87708afb6b1b
ghobson2013 <ugac007@gmail.com>
2018-01-17 06:47:59 -0600

    matched rates to new formula, removed limitation of only earning POS for 30days

--- src/primitives/transaction.cpp
+++ src/primitives/transaction.cpp
@@ -173,30 +173,6 @@ static int ONEYEARPLUS1=ONEYEAR+1;
 static int TWOYEARS=ONEYEAR*2;
 
 static uint64_t rateTable[720*365+1];
-static uint64_t bonusTable[720*365+1];
-
-/*
- * This function validates the coins never to be negative or exceed MAX_MONEY
- */
-CAmount validateCoins(CAmount theAmount){
-    // Never return a negative amount
-    //if(theAmount < 0) { return CAmount(0); }
-
-    // Never return a value larger than MAX_MONEY
-    //if(!MoneyRange(theAmount)) { return MAX_MONEY-1; }
-
-    // Turning off validation since this was never necessary before
-    return theAmount;
-}
-
-CAmount getBonusForAmount(int periods, CAmount theAmount){
-
-    CBigNum amount256(theAmount);
-    CBigNum rate256(bonusTable[periods]);
-    CBigNum rate0256(bonusTable[0]);
-    CBigNum result=(amount256*rate256)/rate0256;
-    return validateCoins(result.getuint64()-theAmount);
-}
 
 CAmount getRateForAmount(int periods, CAmount theAmount){
 
@@ -205,22 +181,7 @@ CAmount getRateForAmount(int periods, CAmount theAmount){
     CBigNum rate0256(rateTable[0]);
     CBigNum result=(amount256*rate256)/rate0256;
 
-    return validateCoins(result.getuint64()-theAmount);
-}
-
-CAmount getPostRateForAmount(int periods, CAmount theAmount){
-
-    if ( periods <= 0 ) { return CAmount(0) ; } // this is needed or we get negative rate
-
-    double result = 0.0;
-    double multiplier = 0.0000005975;
-    for ( int i = 1; i < periods; i++)
-    {
-	result = theAmount * pow(1.0 + multiplier, i);
-    }
-    CAmount rate = CAmount(result)-theAmount;
-
-    return validateCoins(rate);
+    return result.getuint64()-theAmount;
 }
 
 std::string initRateTable()
@@ -228,19 +189,16 @@ std::string initRateTable()
     std::string str;
     rateTable[0]=1;
     rateTable[0]=rateTable[0]<<60;
-    bonusTable[0]=1;
-    bonusTable[0]=bonusTable[0]<<52;
 
     for(int i=1;i<ONEYEARPLUS1;i++)
     {
-        rateTable[i]=rateTable[i-1]+(rateTable[i-1]>>18);  
-        bonusTable[i]=bonusTable[i-1]+(bonusTable[i-1]>>16);
-        str += strprintf("%d %x %x\n",i,rateTable[i], bonusTable[i]);
+        rateTable[i]=rateTable[i-1]+(rateTable[i-1]>>19)+(rateTable[i-1]>>18);
+        str += strprintf("%d %x\n",i,rateTable[i]);
     }
 
     for(int i=0;i<ONEYEAR;i++)
     {
-        str += strprintf("rate: %d %d %d\n",i,getRateForAmount(i,COIN*100),getBonusForAmount(i,COIN*100));
+        str += strprintf("rate: %d %d\n",i,getRateForAmount(i,COIN*100));
     }
 
     return str;
@@ -257,7 +215,8 @@ CAmount GetInterest(CAmount nValue, int outputBlockHeight, int valuationHeight, 
     }
 
     //Regular deposits can have a maximum of 30 days interest
-    int blocks=std::min(THIRTYDAYS,valuationHeight-outputBlockHeight);
+    // TODO REVIEW THIS SHOULD PROBABLY BE REMOVED
+    //int blocks=std::min(THIRTYDAYS,valuationHeight-outputBlockHeight);
 
     // dont allow negative blocks
     blocks=std::max(blocks,0);
@@ -268,42 +227,7 @@ CAmount GetInterest(CAmount nValue, int outputBlockHeight, int valuationHeight, 
         blocks=std::min(ONEYEAR,valuationHeight-outputBlockHeight);
     }
 
-    CAmount standardInterest;
-    if (chainHeight < FORK1HEIGHT || outputBlockHeight < FORK1HEIGHT) 
-    {
-        standardInterest=getRateForAmount(blocks, nValue);
-    }
-    else
-    {
-        standardInterest=getPostRateForAmount(blocks, nValue);
-    }
-
-    CAmount bonusAmount=0;
-
-    if(outputBlockHeight<TWOYEARS && chainHeight < FORK1HEIGHT || outputBlockHeight < FORK1HEIGHT)
-    {
-        //Calculate bonus rate based on outputBlockHeight
-        bonusAmount=getBonusForAmount(blocks, nValue);
-        CBigNum am(bonusAmount);
-        CBigNum fac(TWOYEARS-outputBlockHeight);
-        CBigNum div(TWOYEARS);
-        CBigNum result= am - ((am*fac*fac*fac*fac)/(div*div*div*div));
-        bonusAmount=result.getuint64();
-	//LogPrintf("Pre_Fork: chainHeight: %d lockHeight: %d Principal: %d BonusAmount: %li\n", chainHeight,outputBlockHeight,nValue,bonusAmount);
-    }
-    else if(outputBlockHeight<TWOYEARS && chainHeight >= FORK1HEIGHT)
-    {
-         //Calculate bonus rate based on outputBlockHeight
-         bonusAmount=getBonusForAmount(blocks, nValue);
-         CBigNum am(bonusAmount);
-         CBigNum fac(TWOYEARS);
-         CBigNum div(TWOYEARS);
-         CBigNum result= ((am*fac*fac*fac*fac)/(div*div*div*div))/10; //605% One year Term Deposit Rate
-         bonusAmount=result.getuint64();
-	 //LogPrintf("Post_Fork: chainHeight: %d lockHeight: %d Principal: %d  BonusAmount: %li\n", chainHeight, outputBlockHeight, nValue, bonusAmount);
-    }
-
-    CAmount interestAmount=standardInterest+bonusAmount;
+    CAmount standardInterest = getRateForAmount(blocks, nValue);
     CAmount termDepositAmount=0;
 
     //Reward term deposits more
@@ -313,29 +237,16 @@ CAmount GetInterest(CAmount nValue, int outputBlockHeight, int valuationHeight, 
 
         if(term>720*2)
 	{
-          // Already locked coins benefit from the old pre-fork rate.
-          if(chainHeight < FORK1HEIGHT || outputBlockHeight < FORK1HEIGHT)
-	  {
-             CBigNum am(interestAmount);
-             CBigNum fac(TWOYEARS-term);
-             CBigNum div(TWOYEARS);
-             CBigNum result= am - ((am*fac*fac*fac*fac*fac*fac)/(div*div*div*div*div*div));
-             termDepositAmount=result.getuint64();
-	     //LogPrintf("Pre_Fork: chainHeight: %d lockHeight: %d principal: %d termDepositAmount: %li\n", chainHeight, outputBlockHeight, nValue, termDepositAmount);
-	  }    
-          else
-	  {
-             CBigNum am(interestAmount);
-             CBigNum fac(TWOYEARS-term);
-             CBigNum div(TWOYEARS);
-             CBigNum result= ((am*fac*fac*fac*fac)/(div*div*div*div));
-             termDepositAmount=result.getuint64();
-	     //LogPrintf("Post_Fork: chainHeight: %d lockHeight: %d principal: %d termDepositAmount: %li\n", chainHeight, outputBlockHeight, nValue, termDepositAmount);
-          }
+            CBigNum am(standardInterest);
+            CBigNum fac(TWOYEARS-term);
+            CBigNum div(TWOYEARS);
+            CBigNum result= am - ((am*fac*fac*fac*fac*fac*fac)/(div*div*div*div*div*div));
+            termDepositAmount=result.getuint64();
+	    //LogPrintf("Post_Fork: chainHeight: %d lockHeight: %d principal: %d termDepositAmount: %li\n", chainHeight, outputBlockHeight, nValue, termDepositAmount);
         }
     }
 #ifndef WIN32
-    //LogPrintf("GetInterest: chainHeight: %d lockHeight: %d principal: %d interest: %li pos: %li deposit: %li matureblock: %d\n", chainHeight, outputBlockHeight, nValue, interestAmount, standardInterest, termDepositAmount,maturationBlock);
+    //printf("GetInterest: chainHeight: %d lockHeight: %d principal: %d interest: %li pos: %li deposit: %li matureblock: %d\n", chainHeight, outputBlockHeight, nValue, interestAmount, standardInterest, termDepositAmount,maturationBlock);
 #endif
-    return validateCoins(nValue+interestAmount+termDepositAmount);
+    return nValue+standardInterest+termDepositAmount;
   }
