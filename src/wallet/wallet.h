546bc58a6e10a22ac0d49b5b0af84f875edb65d2
ghobson2013 <ugac007@gmail.com>
2018-01-21 03:25:26 -0600

    show transaction credit without interest, elbandi patch

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -358,7 +358,7 @@ public:
 
     //! filter decides which addresses will count towards the debit
     CAmount GetDebit(const isminefilter& filter) const;
-    CAmount GetCredit(const isminefilter& filter) const;
+    CAmount GetCredit(const isminefilter& filter, bool interest = true) const;
     CAmount GetImmatureCredit(bool fUseCache=true) const;
     CAmount GetImmatureTermDepositCredit() const;
     CAmount GetAvailableCredit(bool fUseCache=true) const;
@@ -656,14 +656,14 @@ public:
     isminetype IsMine(const CTxIn& txin) const;
     CAmount GetDebit(const CTxIn& txin, const isminefilter& filter) const;
     isminetype IsMine(const CTxOut& txout) const;
-    CAmount GetCredit(const CTxOut& txout, const isminefilter& filter, const int& nDepth) const;
+    CAmount GetCredit(const CTxOut& txout, const isminefilter& filter, const int& nDepth, bool interest = true) const;
     bool IsChange(const CTxOut& txout) const;
     CAmount GetChange(const CTxOut& txout) const;
     bool IsMine(const CTransaction& tx) const;
     /** should probably be renamed to IsRelevantToMe */
     bool IsFromMe(const CTransaction& tx) const;
     CAmount GetDebit(const CTransaction& tx, const isminefilter& filter) const;
-    CAmount GetCredit(const CTransaction& tx, const isminefilter& filter, const int& nDepth) const;
+    CAmount GetCredit(const CTransaction& tx, const isminefilter& filter, const int& nDepth, bool interest = true) const;
     CAmount GetChange(const CTransaction& tx) const;
     void SetBestChain(const CBlockLocator& loc);
 
