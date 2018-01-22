6c775a9f077694ce42ba2aa3de4929028dd35ae0
ROI_Coin <dev@roi-coin.com>
2018-01-22 10:42:12 -0600

    Paper Wallet - void printPaperWallets();

--- src/qt/walletframe.h
+++ src/qt/walletframe.h
@@ -72,6 +72,8 @@ public Q_SLOTS:
     void changePassphrase();
     /** Ask for passphrase to unlock wallet temporarily */
     void unlockWallet();
+        
+    void printPaperWallets();
 
     /** Show used sending addresses */
     void usedSendingAddresses();
