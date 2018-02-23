6a0789918338497071d800c88b5580b1ada784b2
ghobson2013 <ugac007@gmail.com>
2018-02-23 00:53:57 -0600

    forgot to merge from openssl pr merge

--- src/crypto/sha512.cpp
+++ src/crypto/sha512.cpp
@@ -2,63 +2,62 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "crypto/sha256.h"
+#include "crypto/sha512.h"
+
 #include "crypto/common.h"
+
 #include <string.h>
 
-////// SHA-256
+////// SHA-512
 
-CSHA256::CSHA256()
+CSHA512::CSHA512()
 {
     this->context = EVP_MD_CTX_new();
     this->initContext();
 }
 
-CSHA256::~CSHA256() {
-    EVP_MD_CTX_free(this->context);
+CSHA512::~CSHA512() {
+   EVP_MD_CTX_free(this->context);
 }
 
-
-CSHA256& CSHA256::Write(const unsigned char* data, size_t len)
+CSHA512& CSHA512::Write(const unsigned char* data, size_t len)
 {
 
-	if (unlikely(!EVP_DigestUpdate(this->context, data, len)))
-            {
-		printf("SHA256: failed to update\n");
-		abort();
-	}
+    if (unlikely(!EVP_DigestUpdate(this->context,data,len))) {
+	printf("SHA512: failed to update digest\n");
+	abort();
+    }
 
     return *this;
 }
 
-void CSHA256::Finalize(unsigned char hash[OUTPUT_SIZE])
+void CSHA512::Finalize(unsigned char hash[OUTPUT_SIZE])
 {
 
-	unsigned int lengthOfHash;
+    unsigned int lengthOfHash;
 
-	if (unlikely(!EVP_DigestFinal_ex(context, hash, &lengthOfHash)))
-                {
-		printf("SHA256: failed to finalize\n");
-		abort();
-	}
+    if (unlikely(!EVP_DigestFinal_ex(this->context, hash, &lengthOfHash)))
+    {
+	printf("SHA512: failed to finalize\n");
+	abort();
+    }
 
 }
 
-CSHA256& CSHA256::Reset()
+CSHA512& CSHA512::Reset()
 {
-	EVP_MD_CTX_reset(this->context);
-	this->initContext();
+    EVP_MD_CTX_reset(this->context); 
+    this->initContext();
+
     return *this;
-
 }
 
-void CSHA256::initContext() {
+void CSHA512::initContext() {
 
-	if (unlikely(!EVP_DigestInit_ex(context, EVP_sha256(), NULL)))
-        {
-		printf("SHA256: failed to init\n");
-		abort();
-	}
+    if (unlikely(!EVP_DigestInit_ex(this->context, EVP_sha512(), NULL))) {
+	printf("SHA512: failed to create digest\n");
+	abort();
+     }
 
 }
 
