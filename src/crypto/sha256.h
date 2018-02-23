d1c36975b3c604267d1aaedb266a936c21d32766
ghobson2013 <ugac007@gmail.com>
2018-02-23 00:47:44 -0600

    forgot to merge from openssl pr merge

--- src/crypto/sha256.h
+++ src/crypto/sha256.h
@@ -7,22 +7,29 @@
 
 #include <stdint.h>
 #include <stdlib.h>
+#include <openssl/sha.h>
+#include <openssl/evp.h>
 
 /** A hasher class for SHA-256. */
 class CSHA256
 {
 private:
-    uint32_t s[8];
-    unsigned char buf[64];
-    size_t bytes;
+
+    EVP_MD_CTX* context;
+
+    void initContext();
 
 public:
-    static const size_t OUTPUT_SIZE = 32;
+    static const size_t OUTPUT_SIZE = SHA256_DIGEST_LENGTH;
 
     CSHA256();
+    ~CSHA256();
+
     CSHA256& Write(const unsigned char* data, size_t len);
     void Finalize(unsigned char hash[OUTPUT_SIZE]);
     CSHA256& Reset();
 };
+
+
 
 #endif // ROICOIN_CRYPTO_SHA256_H
